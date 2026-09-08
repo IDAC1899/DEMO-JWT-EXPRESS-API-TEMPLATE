ae1003af78b7ccf1791872c7538b4ac6242b2a2d
IDAC1899 <isaaldaaysi2@gmail.com>
2026-09-08 10:17:57 +0300

    returned JWT from sign-in route on successful login

--- controllers/authCtrl.js
+++ controllers/authCtrl.js
@@ -46,7 +46,11 @@ router.post('/sign-in', async (req, res) => {
       return res.status(401).json({ err: 'Invalid credentials.' });
     }
 
-    res.status(200).json({ message: 'Signing in!' });
+    const payload = { username: user.username, _id: user._id };
+
+    const token = jwt.sign({ payload }, process.env.JWT_SECRET);
+
+    res.status(200).json({ token });
   } catch (err) {
     res.status(500).json({ err: err.message });
   }
