29cfe8fa85c4eb2a2ea4b423207fe8af00e661e6
IDAC1899 <isaaldaaysi2@gmail.com>
2026-09-08 10:33:49 +0300

    added unprotected user show route

--- controllers/users.js
+++ controllers/users.js
@@ -16,4 +16,17 @@ router.get('/', verifyToken, async (req, res) => {
   }
 });
 
+router.get('/:userId', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    if (!user) {
+      return res.status(404).json({ err: 'User not found.' });
+    }
+
+    res.json({ user });
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
 module.exports = router;
